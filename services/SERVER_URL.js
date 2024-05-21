1b8b59df09778b9433be2a0701dc92eaab3eea65
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-05-21 09:45:12 -0400

    changed to production server

--- services/SERVER_URL.js
+++ services/SERVER_URL.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://localhost:4000'
-// export const SERVER_URL = 'https://isdemocracyserver.adaptable.app' change to server url after deployment
+// export const SERVER_URL = 'http://localhost:4000'
+export const SERVER_URL = 'https://prqtserver.adaptable.app'
